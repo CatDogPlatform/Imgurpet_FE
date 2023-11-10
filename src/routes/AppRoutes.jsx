2749ab31dda0435dd696aa8159a4c4fe2adf9588
bao <qbhoalu@gmail.com>
2023-11-10 15:54:01 +0700

    new

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -6,6 +6,9 @@ import PetStore from "./../pages/store/pet/PetStore";
 import PetDetail from "./../pages/store/pet/PetDetail";
 import GoodStore from "../pages/store/good/GoodStore";
 import GoodDetail from "../pages/store/good/GoodDetail";
+import UserPetList from "./../pages/management/pet/UserPetList";
+import UserGoodlist from "./../pages/management/good/UserGoodList";
+import AddPost from "./../pages/home/AddPost";
 
 const AppRoutes = () => {
     return (
@@ -13,10 +16,13 @@ const AppRoutes = () => {
             <Routes>
                 <Route path="/" element={<LayoutUser />}>
                     <Route path="/" element={<Home />} />
+                    <Route path="/add" element={<AddPost />} />
                     <Route path="/pets" element={<PetStore />} />
                     <Route path="/pets/detail" element={<PetDetail />} />
                     <Route path="/goods" element={<GoodStore />} />
                     <Route path="/goods/detail" element={<GoodDetail />} />
+                    <Route path="/profile/mypets" element={<UserPetList />} />
+                    <Route path="/profile/mygoods" element={<UserGoodlist />} />
                 </Route>
             </Routes>
         </BrowserRouter>
