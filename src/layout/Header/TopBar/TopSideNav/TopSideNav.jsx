f9fbffb0f150cdc1df2a3ba0f3948f08b2329b08
SE151289 <dathtse151289@fpt.edu.vn>
2023-11-10 20:13:04 +0700

    css

--- src/layout/Header/TopBar/TopSideNav/TopSideNav.jsx
+++ src/layout/Header/TopBar/TopSideNav/TopSideNav.jsx
@@ -1,19 +1,33 @@
 import React from "react";
 import "./TopSideNav.scss";
 import { menuItems } from "./menuItem";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 const TopSideNav = () => {
-    return (
-        <div className="nav-menu">
-            {menuItems.map((item) => (
-                <Link to={item.link}>
-                    <div className="nav-menu__item">
-                        <h1 className="item-title">{item.title}</h1>
-                    </div>
-                </Link>
-            ))}
-        </div>
-    );
+  const location = useLocation(); // Sử dụng useLocation để lấy thông tin về URL hiện tại
+  const isActive = (path) => {
+    return location.pathname === path; // So sánh URL hiện tại với path của mỗi thẻ
+  };
+
+  return (
+    <div className="nav-menu">
+      {menuItems.map((item) => (
+        <Link to={item.link}>
+          <div className="nav-menu__item">
+            <h1
+              className="item-title"
+              style={{
+                color: isActive(item.link) ? "#0079FF" : "black",
+                textDecoration: isActive(item.link) ? "underline" : "none",
+                fontWeight: isActive(item.link) ? "bold" : 400,
+              }}
+            >
+              {item.title}
+            </h1>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default TopSideNav;
